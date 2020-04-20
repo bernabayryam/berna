a34a28747d390db78fae17d8addca60ed4b8507b
Berna <63612822+bernabayryam@users.noreply.github.com>
2020-04-20 17:09:23 +0300

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,26 @@
+public class ex1 {
+
+    public static void main (String[] args) {
+        perimeter();
+        area();
+    }
+
+    public static void perimeter(){
+
+        int a=2;
+        int b=6;
+
+        int perimeter = 2 * a + 2*b;
+
+        System.out.println(perimeter);
+    }
+
+    public static void area(){
+        int a=2;
+        int b=6;
+
+        int area = a*b;
+
+        System.out.println(area);
+    }
+}
