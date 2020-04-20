01269811c05b2e5c355e8f9e77547f8abe2d18fb
Berna <63612822+bernabayryam@users.noreply.github.com>
2020-04-20 18:42:39 +0300

    Homework 2

---
+++ exx2.java
@@ -0,0 +1,37 @@
+public class exx2 {
+    public static void main(String[] args) {
+
+        String month = "Януари";
+
+        if (month.equals("Януари")) {
+            System.out.println("31");
+        } else if (month.equals("Февруари")) {
+            System.out.println("29");
+        } else if (month.equals("Март")) {
+            System.out.println("31");
+        } else if (month.equals("Април")) {
+            System.out.println("30");
+        } else if (month.equals("Май")) {
+            System.out.println("31");
+        } else if (month.equals("Юни")) {
+            System.out.println("30");
+        } else if (month.equals("Юли")) {
+            System.out.println("31");
+        } else if (month.equals("Август")) {
+            System.out.println("31");
+        } else if (month.equals("Септември")) {
+            System.out.println("30");
+        } else if (month.equals("Октомври")) {
+            System.out.println("31");
+        } else if (month.equals("Ноември")) {
+            System.out.println("30");
+        } else if (month.equals("Декември")) {
+            System.out.println("31");
+        }
+        else {
+            System.out.println("Грешка");
+        }
+    }
+}
+
+
