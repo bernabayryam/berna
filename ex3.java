c4878387e13ec3cada29e896cdb0428d52e5b387
Berna <63612822+bernabayryam@users.noreply.github.com>
2020-04-20 17:11:29 +0300

    Homework 3

---
+++ ex3.java
@@ -0,0 +1,28 @@
+public class ex3 {
+    public static void main(String[] args){
+        int floors = 9;
+        switch (floors){
+            case 9:
+            case 8:
+                System.out.println("Ани");
+            case 7:
+                System.out.println("Яни");
+            case 6:
+                System.out.println("Еми");
+            case 5:
+                System.out.println("Мими");
+            case 4:
+                System.out.println("Дими");
+            case 3:
+                System.out.println("Ами");
+            case 2:
+                System.out.println("Диди");
+            case 1:
+                System.out.println("Яна");
+            case 0:
+                System.out.println("Ауч");
+
+
+        }
+    }
+}
